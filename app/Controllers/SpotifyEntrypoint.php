45027f09c404f03523c0431ab0ae2a2e42657f08
SebastienBrach <sebastienbrach11@gmail.com>
2022-06-09 18:07:38 +0000

    OK pour refacto spotifyEntrypoint

--- app/Controllers/SpotifyEntrypoint.php
+++ app/Controllers/SpotifyEntrypoint.php
@@ -6,23 +6,43 @@ class SpotifyEntrypoint extends BaseController
 {
     public function index()
     {
-        \Config\Services::session();
-        $session = new \SpotifyWebAPI\Session(
+        $this->setSpotifySession();
+        $this->setSpotifyState();
+        $this->setSpotifyOptions();
+        $this->setCodeIgniterSession();
+        $this->setRedirectionToCallbackSpotify();
+    }
+
+    private function setSpotifySession(){
+        $this->spotifySession = new \SpotifyWebAPI\Session(
             'bbd636c28a7f4b9f875948046b3021f6',
             'f03f9ac87576486d82168c602fd7cea3',
             'http://localhost/auth'
         );
+    }
 
-        $_SESSION['state'] = $session->generateState();
-        $_SESSION['options'] = [
+    private function setSpotifyState(){
+        $this->spotifyState = $this->spotifySession->generateState();
+    }
+
+    private function setSpotifyOptions(){
+        $this->spotifyOptions = [
             'scope' => [
                 'playlist-read-private',
                 'user-read-private',
             ],
-            'state' => $_SESSION['state'],
+            'state' => $this->spotifyState,
         ];
+    }
 
-        header('Location: ' . $session->getAuthorizeUrl($_SESSION['options']));
+    private function setCodeIgniterSession(){
+        \Config\Services::session();
+        $_SESSION['state'] = $this->spotifyState;
+        $_SESSION['options'] = $this->spotifyOptions;
+    }
+
+    private function setRedirectionToCallbackSpotify(){
+        header('Location: ' . $this->spotifySession->getAuthorizeUrl($this->spotifyOptions));
         die();
     }
 }
